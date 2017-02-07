3999f577b4d5436273e3216041bd7f710deca7e1
guillemnicolau <guillemnicolau@gmail.com>
2017-02-07 09:27:31 +0100

    Update inference_pycompss.py
    Final code.

--- inference_pycompss.py
+++ inference_pycompss.py
@@ -1,14 +1,12 @@
 """#"""
 
 import os
-import time
 import argparse
 import tensorflow as tf
 from pycompss.api.task import task
 from pycompss.api.parameter import OUT
 
 NUM_CORES = 1
-VARS = False
 DISABLE_OPTIMIZATION = True
 
 def load_graph(frozen_graph_filename):
@@ -29,7 +27,9 @@ def load_graph(frozen_graph_filename):
   return graph
 
 @task(res=OUT, returns=int)
-def main(path_img, res):
+def main(path, imagepath, img, res):
+  image = str(imagepath + img)
+  modelpath = str(path + 'models/frozen_model.pb')
   config = tf.ConfigProto(
       inter_op_parallelism_threads=NUM_CORES,
       intra_op_parallelism_threads=NUM_CORES
@@ -39,12 +39,12 @@ def main(path_img, res):
       config.graph_options.optimizer_options.opt_level = \
         config.graph_options.optimizer_options.L0
 
-  graph = load_graph(path + '/models/frozen_model.pb')
+  graph = load_graph(modelpath)
   x = graph.get_tensor_by_name('image_path:0')
   y = graph.get_tensor_by_name('output:0')
 
   with tf.Session(graph=graph, config=config) as sess:
-      res = sess.run(tf.squeeze(y), feed_dict={x: path_img})
+      res = sess.run(tf.squeeze(y), feed_dict={x: image})
 
   return res
 
@@ -52,18 +52,15 @@ def main(path_img, res):
 if __name__ == '__main__':
   from pycompss.api.api import compss_wait_on
   cwd = os.getcwd()
-  print "current working directory:", cwd
-  path = cwd + '/input/'
-  print "files path:               ", path
+  path = cwd + '/test_tf/'
+  imagepath = path + 'input/'
   results = []
   filenames = []
-  for filename in os.listdir(path):
+  for filename in os.listdir(imagepath):
       filenames.append(filename)
-      results.append(main(str(path) + str(filename), 1))
+      results.append(main(str(path), str(imagepath), str(filename), 1))
 
   results = compss_wait_on(results)
 
   for ind in range(0, len(filenames)):  
       print("result of " + str(filenames[ind]) + ": " + str(results[ind]))
-
-
